74fe06a49caccb4611026b5c4312e76a40061259
Abhisekhmallah <abhisekhmallah9@gmail.com>
2025-09-22 21:38:47 +0530

    heading

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
                     animate={{ opacity: 1, x: 0 }}
                     transition={{ duration: 0.8, delay: 0.8 }}
                   >
-                    <AuroraText>Fullstack Developer</AuroraText>
+                    <AuroraText> I'm a FullStack Developer</AuroraText>
                   </motion.span>
                 </motion.h1>
               </motion.div>
